3418a9a8bc12f0deca59a8193f5da34394690613
Maximiliano Furtado <maximiliano.oliveira.furtado@gmail.com>
2024-12-02 14:14:57 -0300

    refac: run in my local env

--- fase-4/src/main.py
+++ fase-4/src/main.py
@@ -3,8 +3,8 @@ from face_recognition.face_detection import detect_faces
 
 if __name__ == "__main__":
 
-    video_input = os.path.abspath("fase-4/assets/data/inputs/short_version.mp4")
-    video_output = os.path.abspath("fase-4/assets/data/outputs/processed_video.mp4")
+    video_input = os.path.abspath("assets/data/inputs/short_version.mp4")
+    video_output = os.path.abspath("assets/data/outputs/processed_video.mp4")
 
     print(f"Input video: {video_input}")
     detect_faces(video_input, video_output)
